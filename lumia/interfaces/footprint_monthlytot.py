88b12f0a1e9b8dfb36f351e029284e4cf16c6dca
Guillaume Monteil (guillaume.monteil@nateko.lu.se) <Guillaume Monteil (guillaume.monteil@nateko.lu.se)>
2019-11-19 17:31:36 +0100

    - conversion of "interface" from a module to a package - interfaces/__init__.py contains a code to choose automatically an interface - cleanup in interfaces (removal of options, now it implements only one approach to aggregate the fluxes in control vector). - interfaces also now saves anciliary variables (high-resolution prior)

--- lumia/interfaces/footprint_monthlytot.py
+++ lumia/interfaces/footprint_monthlytot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from .Tools import Region, Categories, colorize
+from lumia.Tools import Region, Categories, colorize
 from numpy import *
 import logging
 import os
@@ -7,73 +7,72 @@ from netCDF4 import Dataset
 from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
 from copy import deepcopy
+from pandas import DataFrame
 
-    
+obsoperator = 'lagrange'
+invcontrol = 'monthlytot'
+
 class Interface:
     data_initialized = False
-    def __init__(self, rcf, struct=None):
+
+    def __init__(self, rcf, ancilliary=None):
         self.rcf = rcf
         self.categories = Categories(rcf)
-        if struct is not None :
-            self.setup(struct, info=True)
         self.writeStruct = WriteStruct
-        
-    def setup(self, struct=None, info=False):
-        self.data = struct
-        if info :
-            for cat in self.categories :
-                logging.info('Total for category %s: %i umol/m2/s'%(cat, self.data[cat.name]['emis'].sum()))
-        self.data_initialized = True
-        
-    def StructToVec(self, struct=None, method='MonthlyFlux'):
-        if struct is None and self.data_initialized :
-            struct = self.data
-        else :
-            logging.critical("No data in memory to form a control vector ...")
-            raise RuntimeError
-        
-        self._stv = getattr(self, '_stv_%s'%method) # TODO: check if the "self" can be avoided here ...
-        self.VecToStruct = getattr(self, '_vts_%s'%method)
-        self.VecToStruct_adj = getattr(self, '_vts_%s_adj'%method)
-        return self._stv(struct)
+        self.region = Region(rcf)
+        self.ancilliary_data = ancilliary
 
-    ###################################
-    # Specific classes
-    
-    def _stv_MonthlyFlux(self, struct):
-        statevec = []
+    def StructToVec(self, struct):
+        vec = DataFrame(columns=['category', 'value'])
+        statevec, categ, lat, lon, time = [], [], [], [], []
+
         for cat in [x for x in self.categories if x.optimize]:
             emcat = coarsenTime(struct[cat.name], cat.optimization_interval, compute_std=False)
+
+            # Prepare time coordinates array
+            times = emcat['time_interval']['time_start'] + (emcat['time_interval']['time_end'] - emcat['time_interval']['time_start'])/2
+
+            # Loop over all the components of the control vector
             for i_time, tt in enumerate(sorted(emcat['time_interval']['time_start'])):
                 for i_lat in range(emcat['emis'].shape[1]):
                     for i_lon in range(emcat['emis'].shape[2]):
                         statevec.append(emcat['emis'][i_time, i_lat, i_lon])
-        return array(statevec)
-    
-    def _vts_MonthlyFlux(self, vector):
-        # TODO: split this in two, with the refining in a second time?
+                        categ.append(cat)
+                        lat.append(self.region.lats[i_lat])
+                        lon.append(self.region.lons[i_lon])
+                        time.append(times[i_time])
+
+        # Store and return
+        vec.loc[:, 'category'] = array(categ, dtype=str)
+        vec.loc[:, 'value'] = array(statevec, dtype=float64)
+        vec.loc[:, 'lat'] = array(lat, dtype=float64)
+        vec.loc[:, 'lon'] = array(lon, dtype=float64)
+        vec.loc[:, 'time'] = array(time)
+        return vec
+
+    def VecToStruct(self, vector):
         i_state = 0
-        
-        fine_data = deepcopy(self.prior_data)
-        for cat in [x for x in catInfo if x.optimize]:
-            coarse_data = coarsenTime(self.prior_data[cat.name], cat.optimization_interval, compute_std=False)
+
+        fine_data = deepcopy(self.ancilliary_data)
+        for cat in [x for x in self.categories if x.optimize]:
+            coarse_data = coarsenTime(fine_data[cat.name], cat.optimization_interval, compute_std=False)
             for i_time, tt in enumerate(sorted(coarse_data['time_interval']['time_start'])):
-                for i_lat in range(data['emis'].shape[1]):
-                    for i_lon in range(data['emis'].shape[2]):
-                        coarse_data['emis'][i_time, i_lat, i_lon] = statevec[i_state]
+                for i_lat in range(coarse_data['emis'].shape[1]):
+                    for i_lon in range(coarse_data['emis'].shape[2]):
+                        coarse_data['emis'][i_time, i_lat, i_lon] = vector[i_state]
                         i_state += 1
             fine_data[cat.name]['emis'] = refineTime(coarse_data, fine_data[cat.name])
         return fine_data
-    
-    def _vts_MonthlyFlux_adj(self, adjstruct):
-        dt = {'y':relativedelta(years=1), 'm':relativedelta(months=1), 'd':timedelta(1)}
+
+    def VecToStruct_adj(self, adjstruct):
+        dt = {'y': relativedelta(years=1), 'm': relativedelta(months=1), 'd': timedelta(1)}
         adjvec = array(())
 
         for cat in [x for x in self.categories if x.optimize]:
-            #1) Adjoint of refineTime (#TODO: take this out of stateToStruct_adj)
+            # 1) Adjoint of refineTime (#TODO: take this out of stateToStruct_adj)
             adjCat = refineTime_adj(
                 deepcopy(adjstruct[cat.name]),
-                self.prior_data[cat.name],
+                self.ancilliary_data[cat.name],
                 dt[cat.optimization_interval]
             )
 
@@ -86,20 +85,19 @@ class Interface:
 
         return adjvec
 
-            
-    
+
 def coarsenTime(emis, interval, compute_std=False):
     intervals_emis = emis['time_interval']['time_start']
-    if interval == 'y' :
+    if interval == 'y':
         intervals_optim = [datetime(x.year, 1, 1) for x in intervals_emis]
         dt = relativedelta(years=1)
     elif interval == 'm':
         intervals_optim = [datetime(x.year, x.month, 1) for x in intervals_emis]
         dt = relativedelta(months=1)
-    elif interval == 'd' :
+    elif interval == 'd':
         intervals_optim = [datetime(x.year, x.month, x.day) for x in intervals_emis]
         dt = timedelta(1)
-    else :
+    else:
         raise NotImplementedError
     emisOut = []
     emisOut_std = []
@@ -107,21 +105,22 @@ def coarsenTime(emis, interval, compute_std=False):
     emisOut_max = []
     intervals_optim = array(intervals_optim)
     for tt in unique(intervals_optim):
-        try :
+        try:
             emisOut.append(emis['emis'][intervals_optim == tt, :, :].sum(0))
-        except :
-            import pdb; pdb.set_trace()
+        except:
+            import pdb;
+            pdb.set_trace()
         nt = sum(intervals_optim == tt)
-        if compute_std :
-            emisOut_std.append(emis['emis'][intervals_optim == tt, :, :].std(0)*nt)
-            emisOut_min.append(emis['emis'][intervals_optim == tt, :, :].min(0)*nt)
-            emisOut_max.append(emis['emis'][intervals_optim == tt, :, :].max(0)*nt)
-    em = {'time_interval':{}}
+        if compute_std:
+            emisOut_std.append(emis['emis'][intervals_optim == tt, :, :].std(0) * nt)
+            emisOut_min.append(emis['emis'][intervals_optim == tt, :, :].min(0) * nt)
+            emisOut_max.append(emis['emis'][intervals_optim == tt, :, :].max(0) * nt)
+    em = {'time_interval': {}}
     em['emis'] = array(emisOut)
     em['time_interval']['time_start'] = unique(intervals_optim)
-    em['time_interval']['time_end'] = unique(intervals_optim)+dt
-    if compute_std :
-        return em, {'std':array(emisOut_std), 'min':array(emisOut_min), 'max':array(emisOut_max)}
+    em['time_interval']['time_end'] = unique(intervals_optim) + dt
+    if compute_std:
+        return em, {'std': array(emisOut_std), 'min': array(emisOut_min), 'max': array(emisOut_max)}
     return em
 
 
@@ -129,7 +128,7 @@ def refineTime(coarseEmis, fineEmis):
     """
     Restore the high resolution temporal structure of the fluxes, from that of a previous time step.
     With:
-        - x the monthly total flux at one grid point, 
+        - x the monthly total flux at one grid point,
 	- f(t) the flux at time step t,
 	- nt the number of time steps in a month
 	- f0(t) the previous (or prior, it does not matter) flux at time step t:
@@ -145,15 +144,15 @@ def refineTime(coarseEmis, fineEmis):
     for i_time in range(nt_optim):
         tmin = coarseEmis['time_interval']['time_start'][i_time]
         tmax = coarseEmis['time_interval']['time_end'][i_time]
-        select = (tstart_emis >= tmin)*(tstart_emis < tmax)
-        nt = sum(select)+0.   # Make sure we have a real
+        select = (tstart_emis >= tmin) * (tstart_emis < tmax)
+        nt = sum(select) + 0.  # Make sure we have a real
         x1 = coarseEmis['emis'][i_time, :, :]
         f0 = fineEmis['emis'][select, :, :]
         x0 = f0.sum(0)
-        f['emis'][select, :, :] = x1/nt + f0 - x0/nt
+        f['emis'][select, :, :] = x1 / nt + f0 - x0 / nt
 
-#	offset = fineEmis['emis'][select, :, :]-fineEmis['emis'][select, :, :].mean(0)
-#	fineEmis['emis'][select, :, :] = coarseEmis['emis'][i_time, :, :]/nt + offset
+    #	offset = fineEmis['emis'][select, :, :]-fineEmis['emis'][select, :, :].mean(0)
+    #	fineEmis['emis'][select, :, :] = coarseEmis['emis'][i_time, :, :]/nt + offset
     return f['emis']
 
 
@@ -163,13 +162,13 @@ def refineTime_adj(adjEmis, fineEmis, dt_optim):
         intervals_optim = array([datetime(x.year, 1, 1) for x in intervals_adjEmis])
     elif dt_optim == relativedelta(months=1):
         intervals_optim = array([datetime(x.year, x.month, 1) for x in intervals_adjEmis])
-    elif dt_optim == timedelta(1) :
+    elif dt_optim == timedelta(1):
         intervals_optim = array([datetime(x.year, x.month, x.day) for x in intervals_adjEmis])
-    else :
+    else:
         raise NotImplementedError
-    for i_time, time in enumerate(unique(intervals_optim)) :
+    for i_time, time in enumerate(unique(intervals_optim)):
         select = intervals_optim == time
-        nt = sum(select)+0.  # Make sure we have a real
+        nt = sum(select) + 0.  # Make sure we have a real
         adjEmis['emis'][select, :, :] /= nt
     return adjEmis
 
@@ -180,12 +179,12 @@ def WriteStruct(data, path, prefix):
     """
 
     # Create the filename and directory (if needed)
-    filename = os.path.join(path, '%s.nc'%prefix)
+    filename = os.path.join(path, '%s.nc' % prefix)
     if not os.path.exists(path):
         os.makedirs(path)
 
     # Write to a netCDF format
-    with Dataset(filename, 'w') as ds :
+    with Dataset(filename, 'w') as ds:
         ds.createDimension('time_components', 6)
         for cat in data.keys():
             gr = ds.createGroup(cat)
@@ -202,13 +201,13 @@ def WriteStruct(data, path, prefix):
 
 
 def ReadStruct(filename):
-    with Dataset(filename) as ds :
+    with Dataset(filename) as ds:
         categories = ds.groups.keys()
-        data = {'cat_list':categories}
-        for cat in categories :
+        data = {'cat_list': categories}
+        for cat in categories:
             data[cat] = {
-                'emis':ds.groups[cat].variables['emis'][:],
-                'time_interval':{
+                'emis': ds.groups[cat].variables['emis'][:],
+                'time_interval': {
                     'time_start': array([datetime(*x) for x in ds.groups[cat].variables['times_start'][:]]),
                     'time_end': array([datetime(*x) for x in ds.groups[cat].variables['times_end'][:]]),
                 }
