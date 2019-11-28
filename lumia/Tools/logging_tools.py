e2e4b340f3bdf0803b435efbbcd734ba883d762d
Guillaume Monteil (guillaume.monteil@nateko.lu.se) <Guillaume Monteil (guillaume.monteil@nateko.lu.se)>
2019-11-28 16:13:22 +0100

    fixed logging so that it works well with tqdm

--- lumia/Tools/logging_tools.py
+++ lumia/Tools/logging_tools.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 import logging
-
+from tqdm.autonotebook import tqdm
 import colorlog
 import shutil
 columns = shutil.get_terminal_size().columns
 
+class TqdmHandler(colorlog.StreamHandler):
+    def __init__(self):
+        colorlog.StreamHandler.__init__(self)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.write(msg)  # , file=sys.stderr)
+       #     self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
 log = logging.getLogger()
 handler = colorlog.StreamHandler()
 formatter = colorlog.ColoredFormatter(
-    "%(message_log_color)s %(name)40s | %(reset)s %(log_color)s %(levelname)-8s (line %(lineno)d) | %(reset)s %(message)s",
+    "%(message_log_color)s %(name)30s | %(reset)s %(log_color)s %(levelname)-8s (line %(lineno)d) | %(reset)s %(message)s",
     datefmt=None,
     reset=True,
     log_colors={
@@ -26,5 +40,10 @@ formatter = colorlog.ColoredFormatter(
 
 )
 handler.setFormatter(formatter)
-log.addHandler(handler)
+
+handler2 = TqdmHandler()
+handler2.setFormatter(formatter)
+
+#log.addHandler(handler)
+log.addHandler(handler2)
 log.setLevel(logging.INFO)
