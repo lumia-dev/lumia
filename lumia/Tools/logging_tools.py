0ae0f6e644fe06bbc7d0fc7a8d4651304f47f1e4
Guillaume Monteil (guillaume.monteil@nateko.lu.se) <Guillaume Monteil (guillaume.monteil@nateko.lu.se)>
2019-12-10 18:24:33 +0100

    Optionalization of colorlog (untested, but it should work ...)

--- lumia/Tools/logging_tools.py
+++ lumia/Tools/logging_tools.py
@@ -2,7 +2,6 @@
 import logging
 from lumia import tqdm
 #from tqdm.autonotebook import tqdm
-import colorlog
 import shutil
 columns = shutil.get_terminal_size().columns
 
@@ -35,45 +34,54 @@ def colorize(msg, color=None):
     msg = msg.replace('</w>', '\x1b[0m')
     return msg
 
-class TqdmHandler(colorlog.StreamHandler):
+try :
+    import colorlog
+    base_handler = colorlog.StreamHandler
+    formatter = colorlog.ColoredFormatter(
+        "%(message_log_color)s %(name)30s | %(reset)s %(log_color)s %(levelname)-8s (line %(lineno)d) | %(reset)s %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            'DEBUG': 'purple',
+            'INFO': 'cyan',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'white,bg_red'},
+        secondary_log_colors={'message': {
+            'DEBUG': 'bold_blue',
+            'INFO': 'bold_cyan',
+            'WARNING': 'bold_yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'white,bg_red'}},
+
+    )
+except :
+    base_handler = logging.StreamHandler
+    formatter = logging.Formatter(
+        "%(name)30s | %(levelname)-8s (line %(lineno)d) | %(message)s",
+        datefmt=None
+    )
+
+class TqdmHandler(base_handler):
     def __init__(self):
-        colorlog.StreamHandler.__init__(self)
+        super(self).__init__()
 
     def emit(self, record):
         try:
             msg = self.format(record)
-            tqdm.write(msg)  # , file=sys.stderr)
-       #     self.flush()
+            tqdm.write(msg)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             self.handleError(record)
 
-log = logging.getLogger()
-handler = colorlog.StreamHandler()
-formatter = colorlog.ColoredFormatter(
-    "%(message_log_color)s %(name)30s | %(reset)s %(log_color)s %(levelname)-8s (line %(lineno)d) | %(reset)s %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
-        'DEBUG': 'purple',
-        'INFO': 'cyan',
-        'WARNING': 'yellow',
-        'ERROR': 'red',
-        'CRITICAL': 'white,bg_red'},
-    secondary_log_colors={'message': {
-        'DEBUG': 'bold_blue',
-        'INFO': 'bold_cyan',
-        'WARNING': 'bold_yellow',
-        'ERROR': 'red',
-        'CRITICAL': 'white,bg_red'}},
+#handler = hl()
+#handler.setFormatter(formatter)
 
-)
+handler = TqdmHandler()
 handler.setFormatter(formatter)
 
-handler2 = TqdmHandler()
-handler2.setFormatter(formatter)
-
 #log.addHandler(handler)
-log.addHandler(handler2)
+log = logging.getLogger()
+log.addHandler(handler)
 log.setLevel(logging.INFO)
