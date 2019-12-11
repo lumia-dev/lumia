646d240452874e1ee6ee3a1af4e6e55c7276c4c1
Guillaume <guillaume.monteil@nateko.lu.se>
2019-12-11 16:49:16 +0100

    Some fixes for MPI preconditioning to work on tetralith + transmission of the logging level to preco subprocesses

--- lumia/Tools/system_tools.py
+++ lumia/Tools/system_tools.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
-import os
+import os, logging
+from lumia.Tools.logging_tools import colorize
+logger = logging.getLogger(__name__)
+import inspect
 
-def checkDir(dirname):
-    if not os.path.exists(dirname): os.makedirs(dirname)
+def checkDir(dirname, silent=False):
+    if not os.path.exists(dirname): 
+        os.makedirs(dirname)
+        if not silent :
+            frame = inspect.stack()[1]
+            mod = inspect.getmodule(frame[0]).__name__
+            lin = frame[2]
+            logger.info(colorize(f"Create path <s>{dirname}</s> (called by {mod} at line {2})"))
