aec6008efd18d524f70c20c0eda175de2184ef44
lumia <70275228+lumia-dev@users.noreply.github.com>
2020-09-11 12:15:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
         name="lumia",
         version="0.0.1",
         author="Guillaume Monteil",
         author_email="guillaume.monteil@nateko.lu.se",
-        description=long_description,
+        description="",
         long_description_content_type="text/markdown",
         url="https://www.geosci-model-dev-discuss.net/gmd-2019-227/#discussion",
         packages=setuptools.find_packages(),
