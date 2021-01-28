d04411f7a0fb8972d376607246c337e2f2522f18
gmonteil <guillaume.monteil@nateko.lu.se>
2021-01-28 10:20:13 +0100

    optimization in units of umol

--- lumia/formatters/lagrange.py
+++ lumia/formatters/lagrange.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 from numpy import unique, append
 import xarray as xr
 from pandas import Timestamp
+from lumia.Tools.regions import region
 from archive import Archive
 logger = logging.getLogger(__name__)
 
@@ -15,6 +16,7 @@ logger = logging.getLogger(__name__)
 class Struct(dict):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.unit_type = 'intensive'
 
     def __add__(self, other):
         allcats = set(list(self.keys())+list(other.keys()))
@@ -54,6 +56,26 @@ class Struct(dict):
                 'lons':self[cat]['lons']
             }
         return outstruct
+
+    def to_extensive(self):
+        assert self.unit_type == 'intensive'
+        for cat in self.keys():
+            dt = self[cat]['time_interval']['time_end']-self[cat]['time_interval']['time_start']
+            dt=array([t.total_seconds() for t in dt])
+            area = region(longitudes=self[cat]['lons'], latitudes=self[cat]['lats']).area
+            self[cat]['emis'] *= area[None, :, :]
+            self[cat]['emis'] *= dt[:, None, None]
+        self.unit_type = 'extensive'
+
+    def to_intensive(self):
+        #assert self.unit_type == 'extensive'
+        for cat in self.keys():
+            dt = self[cat]['time_interval']['time_end']-self[cat]['time_interval']['time_start']
+            dt=array([t.total_seconds() for t in dt])
+            area = region(longitudes=self[cat]['lons'], latitudes=self[cat]['lats']).area
+            self[cat]['emis'] /= area[None, :, :]
+            self[cat]['emis'] /= dt[:, None, None]
+        self.unit_type = 'intensive'
 
     def append(self, other, overwrite=True):
         """
@@ -246,4 +268,7 @@ def ReadArchive(prefix, start, end, **kwargs):
             'lats':ds.lat[:],
             'lons':ds.lon[:]
         }
+
+    if kwargs.get('extensive_units', False) : 
+        data.to_extensive()
     return data
