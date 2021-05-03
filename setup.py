f4e5a7cc9b1e2e8ffc1dcc408cea5885259d2242
gmonteil <guillaume.monteil@nateko.lu.se>
2021-05-03 18:11:24 +0200

    New "main" script

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    scripts=['transport/lagrange_mp.py'],
+    scripts=['transport/lumia.py'],
     data_files=[]
 )
