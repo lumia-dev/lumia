5bfaa2ea76c2641d40a5065a0ffa31158cc73b77
gmonteil <guillaume.monteil@nateko.lu.se>
2022-06-14 14:34:46 +0200

    bugfixes

--- transport/multitracer.py
+++ transport/multitracer.py
@@ -89,11 +89,11 @@ class Observations(DataFrame):
         # 2) Append file names, both for local and archive
         if local is None:
             local = archive
-        fnames_archive = archive + self.footprint
-        fnames_local = archive + self.footprint
+        fnames_archive = archive + '/' + self.footprint
+        fnames_local = local + '/' + self.footprint
 
         # 3) retrieve the files from archive if needed:
-        exists = array([check_migrate(arc, loc) for (arc, loc) in tqdm(zip(fnames_archive, fnames_local), desc='Migrate footprint files')])
+        exists = array([check_migrate(arc, loc) for (arc, loc) in tqdm(zip(fnames_archive, fnames_local), desc='Migrate footprint files', total=len(fnames_local))])
         self.loc[:, 'footprint'] = fnames_local
 
         # Create the file names:
@@ -145,6 +145,7 @@ if __name__ == '__main__':
     p.add_argument('--forward', '-f', action='store_true', default=False, help="Do a forward run")
     p.add_argument('--adjoint', '-a', action='store_true', default=False, help="Do an adjoint run")
     p.add_argument('--footprints', '-p', help="Path where the footprints are stored")
+    p.add_argument('--check-footprints', action='store_true', help='Determine which footprint file correspond to each observation')
     p.add_argument('--copy-footprints', default=None, help="Path where the footprints should be copied during the run (default is to read them directly from the path given by the '--footprints' argument")
     p.add_argument('--adjtest', '-t', action='store_true', default=False, help="Perform and adjoint test")
     p.add_argument('--serial', '-s', action='store_true', default=False, help="Run on a single CPU")
@@ -152,7 +153,7 @@ if __name__ == '__main__':
     p.add_argument('--ncpus', '-n', default=os.cpu_count())
     p.add_argument('--verbosity', '-v', default='INFO')
     p.add_argument('--obs', required=True)
-    p.add_argument('--emis', required=True)
+    p.add_argument('--emis')#, required=True)
     p.add_argument('args', nargs=REMAINDER)
     args = p.parse_args(sys.argv[1:])
 
@@ -160,14 +161,14 @@ if __name__ == '__main__':
     logger.remove()
     logger.add(sys.stderr, level=args.verbosity)
 
-    model = MultiTracer(parallel=not args.serial, ncpus=args.ncpus, tempdir=args.tmp)
 
-    emis = Emissions.read(args.emis)
     obs = Observations.read(args.obs)
 
     if args.check_footprints or 'footprint' not in obs.columns:
         obs.check_footprints(args.footprints, LumiaFootprintFile, local=args.copy_footprints)
 
+    model = MultiTracer(parallel=not args.serial, ncpus=args.ncpus, tempdir=args.tmp)
+    emis = Emissions.read(args.emis)
     if args.forward:
         obs = model.run_forward(obs, emis)
         obs.write(args.obs)
