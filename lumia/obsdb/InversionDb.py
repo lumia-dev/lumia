3d2aed24f9beebc92d09e6db570a7a0bea1375ba
Guillaume Monteil <gmonteil@ik.me>
2023-01-03 13:44:16 +0100

    some config keys name changes in InversionDb.py (if no tracer column is defined, will look for a `observations.file.tracer` key, as opposed to rcf[`run.tracers`][0] up to now.

--- lumia/obsdb/InversionDb.py
+++ lumia/obsdb/InversionDb.py
@@ -22,7 +22,7 @@ class obsdb(obsdb):
     #def __init__(self, rcf, setupUncertainties=True):
 
     @classmethod
-    def from_rc(cls, rcf: Union[dict, rctools.RcFile], setup_uncertainties: bool = True, filekey: str = 'file') -> "obsdb":
+    def from_rc(cls, rcf: Union[dict, rctools.RcFile], setup_uncertainties: bool = True) -> "obsdb":
         """
         Construct an observation database based on a rc-file. The class does the following:
         - load an obs file in tar.gz format
@@ -30,23 +30,24 @@ class obsdb(obsdb):
         - calculate uncertainties (according to how specified in the rc-file)
 
         Arguments:
-            - rcf: rctools.RcFile object
+            - rcf: rctools.RcFile object (or dict)
             - setupUncertainties (optional): determine if the uncertainties need to be computed
-            - filekey (optional): name of the rc-key that point to the file path (default: obs.file)
             - filename (optional): path of the file containing the obs (overrides the one given by filekey)
 
         """
-        db = cls(rcf.get(f'observations.{filekey}'), start=rcf.get('observations.start'), end=rcf.get('observations.end'))
-        db.rcf = rcf
+        db = cls(rcf['observations']['file']['path'], start=rcf['observations']['start'], end=rcf['observations']['end'])
+        # db.rcf = rcf
 
-        db.map_fields(rcf.get(f'observations.{filekey}.rename', default=[]))
+        # Rename fields, if required by the config file or dict:
+        # the config file can have a key "observations.file.rename: col1:col2". In this case, the column "col1" will be renamed in "col2".
+        # this can also be a list of columns: "observations.file.rename: [col1:col2, colX:colY]"
+        db.map_fields(rcf['observations']['file'].get('rename', []))
 
-        # Ensure that the tracer column is present:
+        # If no "tracer" column in the observations file, it can also be provided through the rc-file (observations.file.tracer key)
         if "tracer" not in db.observations.columns:
-            tracers = list(db.rcf.get('tracers'))
-            if len(tracers) == 1:
-                logger.warning(f'No "tracer" column provided. Attributing all observations to tracer {tracers[0]}')
-                db.observations.loc[:, 'tracer'] = tracers[0]
+            tracer = rcf['observations']['file']['tracer']
+            logger.warning(f'No "tracer" column provided. Attributing all observations to tracer {tracer}')
+            db.observations.loc[:, 'tracer'] = tracer
         return db
 
     def setup_uncertainties(self, *args, **kwargs):
