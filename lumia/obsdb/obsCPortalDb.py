74765c7db8d65e0ff4481ebd4e1726c5aa1fc904
isbjoernen <koala.oso@gmail.com>
2023-03-09 10:41:09 +0100

    I reverted lumia/obsdb/inversionDB.py back to the version from 2023-02-14, which is just before I started  working on reading obs data from the carbon portal. Instead a new file in the same directory obsCPortalDb.py is created that will do a dedicated job for reading obs data from the carbon portal.

--- lumia/obsdb/obsCPortalDb.py
+++ lumia/obsdb/obsCPortalDb.py
@@ -24,9 +24,9 @@ class obsdb(obsdb):
     @classmethod
     def from_rc(cls, rcf: Union[dict, rctools.RcFile], setup_uncertainties: bool = True, filekey : str = 'file') -> "obsdb":
         """
-        Construct an observation database based on a rc-file. The class does the following:
-        - load an obs file in tar.gz format
-        - rename fields, if required by the rc-file
+        Construct an observation database based on a rc-file and the carbon portal. The class does the following:
+        - loads all tracer observation files from the carbon portal
+        - rename fields as required
         - calculate uncertainties (according to how specified in the rc-file)
 
         Arguments:
@@ -37,20 +37,24 @@ class obsdb(obsdb):
 
         """
         db = cls(
-            rcf['observations'][filekey]['path'],
-            start=rcf['observations'].get('start', None),
-            end=rcf['observations'].get('end', None))
+            rcf['observations'][filekey]['path'], 
+            start=rcf['observations'].get('start', None), 
+            end=rcf['observations'].get('end', None), 
+            rcf=rcf)
+            #location=rcf['observations'].get('location',  None))
         db.rcf = rcf
 
-        # Rename fields, if required by the config file or dict:
-        # the config file can have a key "observations.file.rename: col1:col2". In this case, the column "col1" will be renamed in "col2".
-        # this can also be a list of columns: "observations.file.rename: [col1:col2, colX:colY]"
-        renameLst=[] # lumia/obsdb/_init_.py expects a List/Dict in map_fields(), not a string
-        renameLst.append(rcf['observations'][filekey].get('rename', []))
-        logger.info('Renaming the following columns in the observations data:')
-        logger.info(renameLst)
-        db.map_fields(renameLst)
-        #db.map_fields(rcf['observations'][filekey].get('rename', []))
+        sLocation=rcf['observations']['file']['location']
+        if ('CARBONPORTAL' not in sLocation):
+            # Rename fields, if required by the config file or dict:
+            # the config file can have a key "observations.file.rename: col1:col2". In this case, the column "col1" will be renamed in "col2".
+            # this can also be a list of columns: "observations.file.rename: [col1:col2, colX:colY]"
+            renameLst=[] # lumia/obsdb/_init_.py expects a List/Dict in map_fields(), not a string
+            renameLst.append(rcf['observations'][filekey].get('rename', []))
+            logger.info('Renaming the following columns in the observations data:')
+            logger.info(renameLst)
+            db.map_fields(renameLst)
+            #db.map_fields(rcf['observations'][filekey].get('rename', []))
 
         # If no "tracer" column in the observations file, it can also be provided through the rc-file (observations.file.tracer key)
         if "tracer" not in db.observations.columns:
@@ -78,13 +82,13 @@ class obsdb(obsdb):
             default_error = self.rcf.get('observations.uncertainty.default_weekly_error')
             if 'err' not in self.sites.columns :
                 self.sites.loc[:, 'err'] = default_error
-
+                
         with Pool() as pp :
             for site in self.sites.itertuples():
                 dbs = self.observations.loc[self.observations.site == site.Index]
                 if dbs.shape[0] > 0 :
                     res.append(pp.apply_async(_calc_weekly_uncertainty, args=(site.code, dbs.time, site.err)))
-
+        
             for r in res :
                 s, e = r.get()
                 self.observations.loc[self.observations.site == s, 'err'] = e
