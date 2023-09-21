5b2fc8784f30333c0f4c01d8ee5a6d30f72e6392
cdgomezo <carlos.gomez@nateko.lu.se>
2023-09-21 13:20:06 +0200

    Update obsdb

--- lumia/obsdb/InversionDb.py
+++ lumia/obsdb/InversionDb.py
@@ -1,44 +1,127 @@
+# #!/usr/bin/env python
+
+# from tqdm import tqdm
+# from datetime import datetime, timedelta
+# from numpy import zeros
+# from lumia.obsdb import obsdb
+# from lumia.Tools.logging_tools import logger
+
+
+# class obsdb(obsdb):
+#     def __init__(self, rcf, setupUncertainties=True):
+#         self.rcf = rcf
+#         start = datetime(*self.rcf.get('time.start'))
+#         end = datetime(*self.rcf.get('time.end'))
+
+#         super().__init__(self.rcf.get('obs.file'), start=start, end=end)
+
+#         if setupUncertainties:
+#             self.SetupUncertainties()
+
+#     def SetupUncertainties(self):
+#         errtype = self.rcf.get('obs.uncertainty')
+#         if errtype == 'weekly':
+#             self.SetupUncertainties_weekly()
+#         elif errtype == 'cst':
+#             self.SetupUncertainties_cst()
+#         else :
+#             logger.error(f'The rc-key "obs.uncertainty" has an invalid value: "{errtype}"')
+#             raise NotImplementedError
+
+#     def SetupUncertainties_weekly(self):
+#         for site in self.sites.itertuples():
+#             dbs = self.observations.loc[self.observations.site == site.Index]
+#             times = dbs.time
+#             err = zeros(len(times)) + site.err
+#             nobs = zeros((len(times)))
+#             for it, tt in tqdm(enumerate(times), desc=site.code, total=dbs.shape[0]):
+#                 nobs[it] = sum((times >= tt-timedelta(days=3.5)) * (times < tt+timedelta(days=3.5)))
+#             err *= nobs**.5
+#             self.observations.loc[self.observations.site == site.Index, 'err'] = err
+
+#     def SetupUncertainties_cst(self):
+#         for site in self.sites.itertuples():
+#             self.observations.loc[self.observations.site == site.Index, 'err'] = site.err
+
+### NEW CODE
+
 #!/usr/bin/env python
 
-from tqdm import tqdm
 from datetime import datetime, timedelta
-from numpy import zeros
+from numpy import zeros, sqrt, isnan, nanmedian
 from lumia.obsdb import obsdb
 from lumia.Tools.logging_tools import logger
+from multiprocessing import Pool
+
+
+infokeys = {
+    'time.start' : 'Earliest date for the observations in the database.',
+    'time.end'   : 'Latest date for the observations in the database.',
+    'obs.file'   : 'Path to the observation file (in tar.gz format).',
+    'obs.fields.rename':'List of columns of the observation file that need to be renamed on import, in the form of "oldname:newname" (where "oldname" is the name of the column in the file. If necessary, a list of fields can be provided (e.g. "colA:colX, colB:colY, colC:colZ".',
+    'obs.uncertainty.setup': 'Determines whether the observation uncertainties need to be computed (default True).',
+    'obs.uncertainty':'Approach used to compute the observation uncertainties. Possible values: "cst" (the observation uncertainties are constant, at each site, and taken after the "err" value in the "sites" table), or "weekly" (the observation uncertainty is set so that the aggregated uncertainties of all the observations in a week matches the "err" value in the "sites" table.'
+}
+
+
+def _calc_weekly_uncertainty(site, times, dbs):
+    # err = zeros(len(times)) + err
+    # nobs = zeros((len(times)))
+    err = zeros(len(times))
+    for it, tt in enumerate(times):
+        # nobs[it] = sum((times >= tt-timedelta(days=3.5)) & (times < tt+timedelta(days=3.5)))
+        err[it] = dbs.obs[(dbs.time >= tt-timedelta(days=3.5)) & (dbs.time < tt+timedelta(days=3.5))].std() * 2 #TODO: Remember to go back to obs instead of old_obs
+    # err *= sqrt(nobs)
+    if  isnan(err).any():
+        err[isnan(err)] = nanmedian(err)
+    return site, err
 
 
 class obsdb(obsdb):
     def __init__(self, rcf, setupUncertainties=True):
         self.rcf = rcf
-        start = datetime(*self.rcf.get('time.start'))
-        end = datetime(*self.rcf.get('time.end'))
+        start = datetime(*self.rcf.get('time.start', info=infokeys))
+        end = datetime(*self.rcf.get('time.end', info=infokeys))
+        super().__init__(self.rcf.get('obs.file', info=infokeys), start=start, end=end)
 
-        super().__init__(self.rcf.get('obs.file'), start=start, end=end)
+        for field in self.rcf.get('obs.fields.rename', tolist='force', default=[], info=infokeys):
+            source, dest = field.split(':')
+            self.observations.loc[:, dest] = self.observations.loc[:, source]
 
-        if setupUncertainties:
-            self.SetupUncertainties()
+        if self.rcf.get('obs.uncertainty.setup', default=setupUncertainties, info=infokeys):
+            for tr in self.rcf.get('obs.tracers'):
+                self.SetupUncertainties(tr)
 
-    def SetupUncertainties(self):
-        errtype = self.rcf.get('obs.uncertainty')
+    def SetupUncertainties(self, tr):
+        errtype = self.rcf.get(f'obs.uncertainty.{tr}') #, info=infokeys)
         if errtype == 'weekly':
-            self.SetupUncertainties_weekly()
+            self.SetupUncertainties_weekly(tr)
         elif errtype == 'cst':
-            self.SetupUncertainties_cst()
+            self.SetupUncertainties_cst(tr)
+        elif errtype == 'tracer':
+            self.SetupUncertainties_tracer(tr)
         else :
             logger.error(f'The rc-key "obs.uncertainty" has an invalid value: "{errtype}"')
             raise NotImplementedError
 
-    def SetupUncertainties_weekly(self):
-        for site in self.sites.itertuples():
-            dbs = self.observations.loc[self.observations.site == site.Index]
-            times = dbs.time
-            err = zeros(len(times)) + site.err
-            nobs = zeros((len(times)))
-            for it, tt in tqdm(enumerate(times), desc=site.code, total=dbs.shape[0]):
-                nobs[it] = sum((times >= tt-timedelta(days=3.5)) * (times < tt+timedelta(days=3.5)))
-            err *= nobs**.5
-            self.observations.loc[self.observations.site == site.Index, 'err'] = err
+    def SetupUncertainties_weekly(self, tr):
+        res = []
+        with Pool() as pp :
+            for site in self.sites.itertuples():
+                dbs = self.observations.loc[(self.observations.site == site.Index) & (self.observations.tracer == tr)]
+                if dbs.shape[0] > 0 :
+                    res.append(pp.apply_async(_calc_weekly_uncertainty, args=(site.code, dbs.time, dbs))) #site._asdict()[f'err_{tr}'])))
+        
+            for r in res :
+                s, e = r.get()
+                self.observations.loc[(self.observations.site == s) & (self.observations.tracer == tr), 'err'] = e
+                logger.info(f"Error for {tr} and site {s:^5s} set to an averge of {e.mean():^8.2f} ppm")
 
-    def SetupUncertainties_cst(self):
-        for site in self.sites.itertuples():
-            self.observations.loc[self.observations.site == site.Index, 'err'] = site.err
+    def SetupUncertainties_cst(self, tr):
+        for site in self.sites.loc[self.sites[tr] == True].itertuples():
+            self.observations.loc[(self.observations.site == site.Index) & (self.observations.tracer == tr), 'err'] = site._asdict()[f'err_{tr}']
+            logger.info(f"Error for {tr} and site {site.Index:^5s} set to an averge of {site._asdict()[f'err_{tr}']:^8.2f} ppm")
+
+    def SetupUncertainties_tracer(self, tr):
+        self.observations.loc[self.observations.tracer == tr, 'err'] = self.observations.loc[self.observations.tracer == tr, 'obs'] * self.rcf.get(f'obs.uncertainty.scale.{tr}', totype=float)
+        logger.info(f"Error for tracer {tr:^5s} set to an averge of {self.observations.loc[self.observations.tracer == tr, 'err'].mean():^8.2f} ppm")
--- lumia/obsdb/__init__.py
+++ lumia/obsdb/__init__.py
@@ -1,3 +1,175 @@
+# import os
+# import shutil
+# import logging
+# import tarfile
+# import tempfile
+# from numpy import unique, nan
+# from pandas import DataFrame, read_csv
+
+# logger = logging.getLogger(__name__)
+
+
+# class obsdb:
+#     def __init__(self, filename=None, start=None, end=None, db=None):
+#         if db is not None :
+#             self._parent = db
+#         else :
+#             self.sites = DataFrame(columns=['code', 'name', 'lat', 'lon', 'alt', 'height', 'mobile'])
+#             self.observations = DataFrame(columns=['time', 'site', 'lat', 'lon', 'alt', 'file'])
+#             self.files = DataFrame(columns=['filename'])
+#             self.start = start
+#             self.end = end
+#             self.setup = False
+#             self.io = {
+#                 'observations':{
+#                     'write':[self.observations.to_csv.__func__, {'path_or_buf':'observations.csv','date_format':'%Y%m%d%H%M%S', 'encoding':'utf8'}],
+#                     'read':(read_csv, {'infer_datetime_format':'%Y%m%d%H%M%S', 'index_col':0, 'parse_dates':['time']}),
+#                     'filename':'observations.csv'
+#                 },
+#                 'sites':{
+#                     'write':[self.sites.to_csv.__func__, {'path_or_buf':'sites.csv','encoding':'utf-8'}],
+#                     'read':(read_csv, {'index_col':0}),
+#                     'filename':'sites.csv',
+#                 },
+#                 'files':{
+#                     'write':[self.files.to_csv.__func__, {'path_or_buf':'files.csv', 'encoding':'utf-8'}],
+#                     'read':(read_csv, {'index_col':0}),# 'engine':'python', 'skipfooter':1}),
+#                     'filename':'files.csv',
+#                 }
+#             }
+#             self.extraFields = {}
+#         if filename is not None :
+#             self.load_tar(filename)
+#             self.filename = filename
+#             if self.start is None :
+#                 self.start = self.observations.time.min()
+#             if self.end is None :
+#                 self.end = self.observations.time.max()
+
+#     def __getattr__(self, item):
+#         if '_parent' in vars(self):
+#             return getattr(self._parent, item)
+#         #else :
+#         #    logger.error(f"Unknown method or attribute for obsdb: {item}")
+#         #    raise AttributeError(item)
+
+#     def __setattr__(self, key, value):
+#         if '_parent' in vars(self):
+#             setattr(self._parent, key, value)
+#         else :
+#             super().__setattr__(key, value)
+
+#     def load_db(self, db):
+#         """
+#         This is a method to import an existing obsdb instance. This enables expanding it with additional methods, from
+#         a derived class. For instance :
+#         db1 = obsdb(filename=db.tar.gz)  # Open an archived database
+#         db2 = footprintdb(db1)           # this will expand the initial "db1" object with methods from the "footprintdb"
+#                                          # class, which is a derived class of obsdb
+#         :param db:
+#         :return:
+#         """
+#         self.observations = db.observations
+#         self.sites = db.sites
+#         self.files = db.files
+
+#     def SelectTimes(self, tmin=None, tmax=None, copy=True):
+#         tmin = self.start if tmin is None else tmin
+#         tmax = self.end if tmax is None else tmax
+#         tmin = self.observations.time.min() if tmin is None else tmin
+#         tmax = self.observations.time.max() if tmax is None else tmax
+#         observations = self.observations.loc[(
+#             (self.observations.time >= tmin) &
+#             (self.observations.time <= tmax)
+#         )]
+#         sites = self.sites.loc[unique(self.observations.site), :]
+#         if copy :
+#             new = obsdb(start=tmin, end=tmax)
+#             new.observations = observations
+#             new.sites = sites
+#             new.files = self.files
+#             return new
+#         else :
+#             self.observations = observations
+#             self.sites = sites
+
+#     def SelectSites(self, sitelist):
+#         selection = [x in sitelist for x in self.observations.site]
+#         self.SelectObs(selection)
+
+#     def SelectObs(self, selection):
+#         self.observations = self.observations.loc[selection,:]
+#         sites = unique(self.observations.site)
+#         self.sites = self.sites.loc[sites]
+#         #if hasattr(self, 'files'):
+#         #    self.files = self.files.loc[unique(self.observations.file)]
+
+#     def get_iloc(self, selection):
+#         db = obsdb()
+#         db.observations = self.observations.iloc[selection]
+#         sites = unique(db.observations.site)
+#         db.sites = self.sites.loc[sites, :]
+#         # Often that part of the database is corrupted, so we try to read it, but we don't try too hard ...
+#         if hasattr(self, 'files') and 'file' in db.observations.columns:
+#             file_indices = unique(db.observations.file.dropna())
+#             files_in_db = [f for f in file_indices if f in db.files.index]
+#             files_not_in_db = [f for f in file_indices if f not in files_in_db]
+#             if len(files_in_db) > 0 :
+#                 db.files = self.files.loc[files_in_db, :]
+#             if len(files_not_in_db) > 0 :
+#                 for file in files_not_in_db :
+#                     db.observations.loc[db.observations.file == file, 'file'] = nan
+#         return db
+
+#     def save_tar(self, filename):
+#         logger.info("Writing observation database to %s", filename)
+
+#         # Create a unique temporary directory, save the current directory
+#         dirname, filename = os.path.split(filename)
+#         dirname = './' if dirname == '' else dirname
+#         import pdb; pdb.set_trace()
+#         if not os.path.exists(os.path.dirname(filename)):
+#             os.makedirs(os.path.dirname(filename))
+#         tmpdir = tempfile.mkdtemp(dir=os.path.dirname(filename))
+#         curdir = os.getcwd()
+#         os.chdir(tmpdir)
+
+#         # Create a tar file (and the intermediate files that go in the tar) in that temporary directory
+#         tmpfile = os.path.basename(filename)
+#         with tarfile.open(tmpfile, 'w:gz') as tar :
+#             for field in self.io :
+#                 method, kwargs = self.io[field]['write']
+#                 method(getattr(self, field), **kwargs)
+#                 tar.add(self.io[field]['filename'])
+#                 os.remove(self.io[field]['filename'])
+
+#         # Move back to the original directory, and move the tarfile in it
+#         os.chdir(curdir)
+#         os.rename(os.path.join(tmpdir, tmpfile), filename)
+
+#         # Delete the temporary directory
+#         shutil.rmtree(tmpdir)
+#         return filename
+
+#     def load_tar(self, filename):
+#         with tarfile.open(filename, 'r:gz') as tar:
+#             for field in self.io :
+#                 method, kwargs = self.io[field]['read']
+#                 data = method(tar.extractfile(self.io[field]['filename']), **kwargs)
+#                 setattr(self, field, data)
+#         self.SelectTimes(self.start, self.end)
+#         logger.info(f"{self.observations.shape[0]} observation read from {filename}")
+
+#     def checkIndex(self, reindex=False):
+#         if True in self.observations.index.duplicated():
+#             if reindex :
+#                 logger.warning("Duplicated indices found in the observations table! The table will be reindexed and the original indices will be lost!")
+#                 self.observations.reset_index(inplace=True)
+#             else :
+#                 logger.error("Duplicated indices found in the observations table!")
+#                 raise RuntimeError
+
+
 import os
 import shutil
 import logging
@@ -15,7 +187,7 @@ class obsdb:
             self._parent = db
         else :
             self.sites = DataFrame(columns=['code', 'name', 'lat', 'lon', 'alt', 'height', 'mobile'])
-            self.observations = DataFrame(columns=['time', 'site', 'lat', 'lon', 'alt', 'file'])
+            self.observations = DataFrame(columns=['time', 'site', 'lat', 'lon', 'alt'])
             self.files = DataFrame(columns=['filename'])
             self.start = start
             self.end = end
@@ -40,6 +212,7 @@ class obsdb:
             self.extraFields = {}
         if filename is not None :
             self.load_tar(filename)
+            self.filename = filename
             if self.start is None :
                 self.start = self.observations.time.min()
             if self.end is None :
@@ -83,7 +256,7 @@ class obsdb:
         )]
         sites = self.sites.loc[unique(self.observations.site), :]
         if copy :
-            new = obsdb(start=tmin, end=tmax)
+            new = self.__class__(start=tmin, end=tmax)
             new.observations = observations
             new.sites = sites
             new.files = self.files
@@ -124,14 +297,16 @@ class obsdb:
         logger.info("Writing observation database to %s", filename)
 
         # Create a unique temporary directory, save the current directory
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-        tmpdir = tempfile.mkdtemp(dir=os.path.dirname(filename))
+        dirname, filename = os.path.split(filename)
+        dirname = './' if dirname == '' else dirname
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        tmpdir = tempfile.mkdtemp(dir=dirname)
         curdir = os.getcwd()
         os.chdir(tmpdir)
 
         # Create a tar file (and the intermediate files that go in the tar) in that temporary directory
-        tmpfile = os.path.basename(filename)
+        tmpfile = filename
         with tarfile.open(tmpfile, 'w:gz') as tar :
             for field in self.io :
                 method, kwargs = self.io[field]['write']
@@ -141,11 +316,11 @@ class obsdb:
 
         # Move back to the original directory, and move the tarfile in it
         os.chdir(curdir)
-        os.rename(os.path.join(tmpdir, tmpfile), filename)
+        os.rename(os.path.join(tmpdir, tmpfile), os.path.join(dirname, filename))
 
         # Delete the temporary directory
         shutil.rmtree(tmpdir)
-        return filename
+        return os.path.join(dirname, filename)
 
     def load_tar(self, filename):
         with tarfile.open(filename, 'r:gz') as tar:
@@ -153,7 +328,7 @@ class obsdb:
                 method, kwargs = self.io[field]['read']
                 data = method(tar.extractfile(self.io[field]['filename']), **kwargs)
                 setattr(self, field, data)
-        self.SelectTimes(self.start, self.end)
+        self.SelectTimes(self.start, self.end, copy=False)
         logger.info(f"{self.observations.shape[0]} observation read from {filename}")
 
     def checkIndex(self, reindex=False):
